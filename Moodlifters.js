91545152f26788dae3b7a9bae1d46ffaef7898e7
JNewton2023 <JonathanNewton8@gmail.com>
2023-06-22 23:29:25 -0400

    updating media page

--- Moodlifters.js
+++
@@ -1,9 +0,0 @@
-<script>
-  function displayVideo(videoId) 
-  {
-    var videoSection = document.getElementById('video-section');
-    videoSection.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/' + videoId + '" frameborder="0" allowfullscreen></iframe>';
-  }
-
-</script>
-
